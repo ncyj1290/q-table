a8d2672512ccd15a2358af0eb8487143277ea052
조영재 <ncyj1290@gmail.com>
2025-09-13 23:55:10 +0900

    첫커밋

---
+++ src/test/java/com/itwillbs/QTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwillbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
