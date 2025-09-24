52692341a6eacc05ee411bcd1802bb75b709506e
SickTiredEngineer <chno336@naver.com>
2025-09-24 15:07:39 +0900

    250924_01
    Test Commit
    조영재 쌉게이

---
+++ src/main/java/com/itwillbs/qtable/controller/YjGayController.java
@@ -0,0 +1,16 @@
+package com.itwillbs.qtable.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class YjGayController {
+
+	@GetMapping("YjGay")
+	public String yjGay() {
+		
+		return "zzzzzzzz";
+	}
+	
+	
+}
