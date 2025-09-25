5c9d1ba90da2a7b37168b89b99bd4e02a1b21387
vodof11 <tmddyd5465@naver.com>
2025-09-25 17:44:33 +0900

    메인 컨트롤러랑 메인 html 나중에 index에 옮기거나 main.html을 기본으로 쓸예정

---
+++ src/main/java/com/itwillbs/qtable/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.itwillbs.qtable.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+	@GetMapping("/main")
+	public String main(){
+		return "main";
+	}
+	
+	
+}
