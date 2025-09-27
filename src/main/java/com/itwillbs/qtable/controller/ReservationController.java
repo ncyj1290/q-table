d92b187c251e7b6a4fb4f11078fd5958a8bdc439
조영재 <ncyj1290@gmail.com>
2025-09-28 02:26:40 +0900

    예약 페이지
    예약페이지 html css

---
+++ src/main/java/com/itwillbs/qtable/controller/ReservationController.java
@@ -0,0 +1,14 @@
+package com.itwillbs.qtable.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+	
+	@GetMapping("reservation")
+	public String reservation() {
+		return "reservation/reservation";
+	}
+	
+}
