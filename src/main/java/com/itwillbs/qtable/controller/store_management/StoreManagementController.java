d34a9f7b3fcecdd1947233ed05de02d0534a728e
SickTiredEngineer <chno336@naver.com>
2025-09-28 16:54:36 +0900

    250928_01
    공통 레이아웃 마진 110 -> 350px
    매장 관리자 - 매장 등록 틀 제작

---
+++ src/main/java/com/itwillbs/qtable/controller/store_management/StoreManagementController.java
@@ -0,0 +1,23 @@
+package com.itwillbs.qtable.controller.store_management;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StoreManagementController {
+
+	
+	/* 매장 관리자가 이용하는 매장 관리 메인 페이지 */
+	@GetMapping("/store_management_main")
+	public String storeManagement() {
+		return "store_management/store_management_main";
+	}
+	
+	/* 매장 등록/수정 페이지 */
+	@GetMapping("/write_store")
+	public String wirteStore() {
+		return "store_management/write_store";
+	}
+	
+	
+}
