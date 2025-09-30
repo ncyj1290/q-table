9dfa2df53737dbd0a127e11aca7dedee47bec825
Deokgyo <ejrry3218@naver.com>
2025-09-30 22:29:15 +0900

    active toggle 구현하기
    active toggle 구현하기

---
+++ src/main/resources/static/js/search/searchSidebar.js
@@ -0,0 +1,45 @@
+$(function() {
+	console.log('searchSidebar js 연동');
+	initializePriceSlider();
+	$('.selectedKeywords:not(.no-toggle)').on('click', '.keyword', function() {
+		toggleActive(this);
+	});
+});
+
+function toggleActive (el) {
+	const parentEl = $(el).closest('.selectedKeywords');
+	if(parentEl.hasClass('single-choice')) $(el).siblings('.keyword').removeClass('active');
+	$(el).toggleClass('active');
+}
+
+// noUiSlider 라이브러리 
+function initializePriceSlider() {
+	// 1. jQuery 셀렉터로 엘리먼트를 선택합니다.
+	const $priceSlider = $('#price-slider');
+	const $sliderValues = $('#slider-values');
+	// 2. noUiSlider를 생성합니다.
+	// noUiSlider는 순수 DOM 엘리먼트를 필요로 하므로, jQuery 객체에서 [0]을 사용해 DOM 엘리먼트를 추출합니다.
+	noUiSlider.create($priceSlider[0], {
+	    start: [0, 40],
+	    connect: true,
+	    step: 1,
+	    range: {
+	        'min': 0,
+	        'max': 40
+	    },
+	    format: {
+	        to: function(value) {
+	            return Math.round(value) + '만원';
+	        },
+	        from: function(value) {
+	            return Number(value.replace('만원', ''));
+	        }
+	    }
+	});
+	// 3. 'update' 이벤트를 리스닝합니다.
+	// $priceSlider[0].noUiSlider는 라이브러리 API에 접근하는 공식적인 방법입니다.
+	$priceSlider[0].noUiSlider.on('update', function(values) {
+	    // 4. jQuery의 .html() 메소드를 사용해 값을 업데이트합니다.
+	    $sliderValues.html(values.join(' ~ '));
+	});
+}
