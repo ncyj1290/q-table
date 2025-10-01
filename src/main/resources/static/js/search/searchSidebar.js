00c5c3ec36a09397323aab6c92ca53f9b63d0aa9
Deokgyo <ejrry3218@naver.com>
2025-10-01 13:45:37 +0900

    사이드바 수정
    슬라이더 세부 기능 구현, 모달창 띄우기

--- src/main/resources/static/js/search/searchSidebar.js
+++ src/main/resources/static/js/search/searchSidebar.js
@@ -1,22 +1,40 @@
 $(function() {
+	
+//	console.log(slider.noUiSlider.get(true)); 값가져오기
 	console.log('searchSidebar js 연동');
+	
+	//슬라이더 라이브러리 초기화
 	initializePriceSlider();
 	
 	//키워드들 클릭했을때
 	$('.selectedKeywords:not(.no-toggle)').on('click', '.keyword', function() {
-		toggleActive(this);
+		toggleActive(this);//active 토글
+		priceUpdate(this);//가격 키워드 반영
 		//쿼리스트링 반영하는것도 짜야함 
 	});
 	
 	//초기화 눌렀을때
 	$('.top').on('click','.reset', function() {
-		removeActive(this);
+		reset(this);
 		//쿼리스트링에서삭제 하느것도 해야함
 	});
 });
 
-function removeActive(el) {
-	let isNoReset = $(el).closest('.top').hasClass('no-reset');
+function priceUpdate(el) {
+	let isPrice = $(el).hasClass('priceKeyword');
+	let isActive = $(el).hasClass('active');
+	if(!isPrice) return;
+	if($(el).hasClass(0) && isActive) $('#price-slider')[0].noUiSlider.set([0,10]);
+	if($(el).hasClass(10) && isActive) $('#price-slider')[0].noUiSlider.set([10,20]);
+	if($(el).hasClass(20) && isActive) $('#price-slider')[0].noUiSlider.set([20,30]);
+	if($(el).hasClass(30) && isActive) $('#price-slider')[0].noUiSlider.set([30,40]);
+	if($(el).hasClass(40) && isActive) $('#price-slider')[0].noUiSlider.set([40,40]);
+	if(!isActive) $('#price-slider')[0].noUiSlider.set([0,40]);
+}
+
+function reset(el) {
+	const isNoReset = $(el).closest('.top').hasClass('no-reset');
+	const isPrice = $(el).closest('.top').hasClass('price');
 	if(isNoReset) {
 		const parentEl = $(el).closest('section.part').find('.selectedKeywords');
 		parentEl.empty();
@@ -24,6 +42,7 @@ function removeActive(el) {
 		$(el).closest('section.part').find('.select').hide();
 		return;	
 	}
+	if(isPrice) $('#price-slider')[0].noUiSlider.reset();
 	$(el).closest('section.part').find('.keyword').removeClass('active');
 }
 
@@ -31,6 +50,10 @@ function toggleActive (el) {
 	const parentEl = $(el).closest('.selectedKeywords');
 	if(parentEl.hasClass('single-choice')) $(el).siblings('.keyword').removeClass('active');
 	$(el).toggleClass('active');
+}
+
+function priceToggle(minValue) {
+	$("."+ minValue).addClass('active');
 }
 
 // noUiSlider 라이브러리 
@@ -58,9 +81,24 @@ function initializePriceSlider() {
 	    }
 	});
 	// 3. 'update' 이벤트를 리스닝합니다.
-	// $priceSlider[0].noUiSlider는 라이브러리 API에 접근하는 공식적인 방법입니다.
 	$priceSlider[0].noUiSlider.on('update', function(values) {
-	    // 4. jQuery의 .html() 메소드를 사용해 값을 업데이트합니다.
-	    $sliderValues.html(values.join(' ~ '));
+		const sliderEl = $priceSlider[0].noUiSlider;
+        const cValues = sliderEl.get(true);
+	    values[0] == '40만원' && values[1] == '40만원' ? $sliderValues.html(values[0]+ '이상')
+													 : $sliderValues.html(values.join(' ~ '));
+	 //키워드랑 슬라이드 값이랑 동기화 시키기 
+		if (cValues[0] == 0 && cValues[1] == 10) priceToggle(cValues[0]);
+		else if (cValues[0] == 10 && cValues[1] == 20) priceToggle(cValues[0]);
+		else if (cValues[0] == 20 && cValues[1] == 30) priceToggle(cValues[0]);
+		else if (cValues[0] == 30 && cValues[1] == 40) priceToggle(cValues[0]);
+		else if (cValues[0] == 40 && cValues[1] == 40) priceToggle(cValues[0]);
+		else $('.priceKeyword').removeClass('active');
 	});
+	
+	$priceSlider[0].noUiSlider.on('slide', function() {
+		const sliderEl = $priceSlider[0].noUiSlider;
+		const cValues = sliderEl.get(true);
+		// 최소 0~1만원
+		if (cValues[0] == 0 && cValues[1] < 1) sliderEl.set([null, 1])
+  	});
 }
