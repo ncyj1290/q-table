441078ff99a2c23140ed1d472ede8760aa218edf
SickTiredEngineer <chno336@naver.com>
2025-10-06 15:18:08 +0900

    251006_03
    FileUploadService

--- src/main/java/com/itwillbs/qtable/controller/storeManagement/StoreWriteController.java
+++ src/main/java/com/itwillbs/qtable/controller/storeManagement/StoreWriteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.itwillbs.qtable.service.FileUploadService;
 import com.itwillbs.qtable.service.storeManagement.StoreWriteService;
 import com.itwillbs.qtable.vo.commonCode.CommonCodeVO;
 import com.itwillbs.qtable.vo.storeManagement.StoreVO;
@@ -18,6 +19,9 @@ public class StoreWriteController {
 	
 	@Autowired
 	StoreWriteService storeWriteService;
+	
+	@Autowired
+	FileUploadService fileUploadService;
 	
 	
 	/* ================================================= */
@@ -34,13 +38,25 @@ public class StoreWriteController {
 	
 	/* 매장 등록 Post */
 	@PostMapping("/write_store")
-	public String inserNewStore(@ModelAttribute  StoreVO storeVO) {
+	public String inserNewStore(@ModelAttribute  StoreVO storeVO) throws Exception {
 		
 		System.out.println("Check Write Store Submit -> Done");
 		System.out.println("Store Write - VO CHECK: " + storeVO.toString());
 		
+		
+		
+		String savePath = fileUploadService.saveFileAndGetPath(storeVO.getStore_profile_file());
+		
+		
+		
+		
 		/* 매장 주소 가공 */
 		storeVO.setFull_address(storeVO.getPost_code() + " " + storeVO.getAddress() + " " + storeVO.getAddress_detail());
+		
+		
+		
+		
+		System.out.println("File Save Path: " + savePath);
 
 		return "redirect:store_reservation_list";
 	}
