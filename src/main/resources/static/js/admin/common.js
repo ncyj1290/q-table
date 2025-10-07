4011799b3cae368cabeab118862d3c7a6324cf48
LEE-DAEEUN <lee0166@gmail.com>
2025-10-08 00:16:14 +0900

    관리자페이지 ajax csrf 적용
    ㅇㅇ

--- src/main/resources/static/js/admin/common.js
+++ src/main/resources/static/js/admin/common.js
@@ -50,3 +50,16 @@ $(function() {
 	});
 });
 
+$(function () {
+    // meta 태그에서 토큰과 헤더 이름 가져오기
+    const token = $("meta[name='_csrf']").attr("content");
+    const header = $("meta[name='_csrf_header']").attr("content");
+
+    // jQuery의 모든 AJAX 요청에 자동으로 CSRF 헤더 추가
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(header, token);
+        }
+    });
+});
+
