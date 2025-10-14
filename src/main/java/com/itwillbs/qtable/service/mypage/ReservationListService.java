029392bca300afcb3e536592a6871427a81e75a1
hayoonjeon123 <gkdbs0309@gmail.com>
2025-10-14 14:37:53 +0900

    헤더 링크 수정
    헤더 링크 수정

--- src/main/java/com/itwillbs/qtable/service/mypage/ReservationListService.java
+++ src/main/java/com/itwillbs/qtable/service/mypage/ReservationListService.java
@@ -16,18 +16,19 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class ReservationLisService {
+public class ReservationListService {
 
 	private final ReservationListMapper reservationListMapper;
 
     /*방문예정 예약 리스트 조회*/
-	public List<Map<String, Object>> getUpcomingList(String MemberIdx) {
-		List<Map<String, Object>> result = reservationListMapper.getMyReservationList(MemberIdx, "rsrt_05");
+	public List<Map<String, Object>> getUpcomingList(String memberIdx) {
+		List<Map<String, Object>> result = reservationListMapper.getMyReservationList(memberIdx, "rsrt_05");
 	    return result;
 	}
 
-	/*취소 예약 리스트 조회*/
-    public List<Map<String, Object>> getCanceledList(String MemberIdx) {
-        return reservationListMapper.getMyReservationList(MemberIdx, "rsrt_03");
-    }
+//	/*취소 예약 리스트 조회*/
+//    public  List<Map<String, Object>> getCanceledList(String memberIdx) {
+//    	List<Map<String, Object>> result = reservationListMapper.getCanceledList(memberIdx, "rsrt_03");
+//	    return result;
+//    }
 }
