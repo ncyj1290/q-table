33a2f791c05008d2ed34f55c05db100c67f120fb
hayoonjeon123 <gkdbs0309@gmail.com>
2025-10-16 00:07:55 +0900

    예약취소 조회
    예약취소 조회

--- src/main/resources/static/js/mypage/mypage_main.js
+++ src/main/resources/static/js/mypage/mypage_main.js
@@ -1,15 +1,34 @@
-function showTab(tabId, element) {
-  // 모든 탭 내용 숨기기
-  document.querySelectorAll(".tab-content").forEach(c => c.style.display = "none");
+let currentStat = 'rsrt_05';  // 디폴트 방문예정 상태
+function showTab(tabId, element, reserveResult) {
+	// 모든 탭 내용 숨기기
+	document.querySelectorAll(".tab-content").forEach(c => c.style.display = "none");
 
-  // 선택한 탭만 보이기
-  document.getElementById(tabId).style.display = "block";
+	// 선택한 탭만 보이기
+	document.getElementById(tabId).style.display = "block";
 
-  // 모든 탭 라벨에서 active 제거
-  document.querySelectorAll(".reserve-label").forEach(l => l.classList.remove("active"));
+	// 모든 탭 라벨에서 active 제거
+	document.querySelectorAll(".reserve-label").forEach(l => l.classList.remove("active"));
 
-  // 클릭한 라벨 활성화
-  element.classList.add("active");
-  
-  
+	// 클릭한 라벨 활성화
+	element.classList.add("active");
+
+	currentStat = reserveResult;  // 현재 상태 갱신
+	// 서버에서 예약 리스트 조회 및 화면에 렌더링
+	$.ajax({
+	  url: "/reservation_list",
+	  type: "GET",
+	  data: { reserveResult: currentStat },
+	  headers: {"X-Requested-With": "XMLHttpRequest"},
+	  success: function(html) {
+	    document.getElementById("reservationContent").innerHTML = html;
+	  },
+	  error: function() {
+	    alert("[translate:데이터 조회가 실패하였습니다.]");
+	  }
+	});
 }
+
+$(document).ready(function() {
+  showTab('visit', document.querySelector('.reserve-label.active'), 'rsrt_05');
+})
+
