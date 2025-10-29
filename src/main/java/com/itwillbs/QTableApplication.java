9f436e7b5bed38575fe58c13bd0ca4a2ee840b29
ncyj1290 <ncyj1290@gmail.com>
2025-10-29 17:41:40 +0900

    배포설정
    배포설정

--- src/main/java/com/itwillbs/QTableApplication.java
+++ src/main/java/com/itwillbs/QTableApplication.java
@@ -2,14 +2,21 @@ package com.itwillbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.*;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling // 스프링 스케줄링
-public class QTableApplication {
-
+public class QTableApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(QTableApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(QTableApplication.class, args);
 	}
