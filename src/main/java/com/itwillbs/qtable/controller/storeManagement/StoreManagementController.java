6cda22f69c2f1b066e9953b97a10aa09d7c218c3
SickTiredEngineer <chno336@naver.com>
2025-11-02 14:59:30 +0900

    251102_01
    배포용 QR로 교체

--- src/main/java/com/itwillbs/qtable/controller/storeManagement/StoreManagementController.java
+++ src/main/java/com/itwillbs/qtable/controller/storeManagement/StoreManagementController.java
@@ -1,11 +1,14 @@
 package com.itwillbs.qtable.controller.storeManagement;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.google.zxing.WriterException;
 import com.itwillbs.qtable.config.QtableUserDetails;
 import com.itwillbs.qtable.service.storeManagement.StoreDataService;
 
